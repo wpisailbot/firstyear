b789338b74500cb48ecb2aba5b6c833314349299
Demo User <debian@beaglebone.localdomain>
2015-03-01 20:47:34 +0000

    Did work to make BBB work--tested in lab.

--- bbb/py/main.py
+++ bbb/py/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from bbio import *
 from math import pi
 from serial_flow import SerialFlow
@@ -7,14 +8,16 @@ from ard_int import ArduinoInterface
 from controls import get_rudder_angle, get_winch_angle
 
 flow_pin = None
-winch_pwm = PWM1A
-rudder_pwm = PWM1B
+winch_pwm = PWM1B
+rudder_pwm = PWM1A
 def setup():
 
   # XBee setup
   global xbee_com
+  global xbee_ser
   Serial2.begin(9600)
   xbee_com = SerialFlow(Serial2, flow_pin)
+  xbee_ser = Serial2
 
   # Winch and Rudder setup
   global winch, rudder
@@ -34,10 +37,12 @@ def setup():
   arduino = ArduinoInterface(Serial1)
 
 def loop():
+  global xbee_ser
   global gps_reader
   lat, lon = gps_reader.get_pos()
   print "Latitude, Longitude:"
   print lat, ", ", lon
+  xbee_ser.prints("%f, %f\n" % (lat, lon))
 
   global arduino
   rel_wind = arduino.get_wind()
@@ -50,7 +55,14 @@ def loop():
 
   global winch, rudder
   heading = 0
-  winch.set(get_winch_angle(rel_wind))
-  rudder.set(get_rudder_angle(heading, yaw, rel_wind * pi / 180.));
+  winch_val = get_winch_angle(rel_wind)
+  rudder_val = get_rudder_angle(heading, yaw, rel_wind * pi / 180.)
+  print "Winch, Rudder"
+  print winch_val, rudder_val
+  winch.set(winch_val)
+  rudder.set(rudder_val)
+
+  xbee_ser.prints("HELLO, WORLD!!!!" + repr(lon));
+  delay(20)
 
 run(setup, loop)
