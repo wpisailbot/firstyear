b7457001848024422a4d3e1bed598d3a1ac60bcc
James Kuszmaul <jabukuszmaul+collab@gmail.com>
2016-03-31 17:44:19 -0400

    Added flow control for X-Bee.
    I haven't tested the flow control with the GPS running yet.
    I have tested the wind sensor (I added a calibration routine at the beginning).

---
+++ sensor_test/SerialFlow.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "Arduino.h"
+
+class SerialFlow {
+ /**
+  * The SerialFlow class is meant for use with an X-bee, where
+  * when you write data to the xbee, it is possible to fill up the
+  * buffer.
+  * On the X-bee, when there are 17 bytes remaining in the buffer,
+  * the CTS pin will go high. Once there are 34 bytes free again,
+  * the CTS pin will re-assert low.
+  * See https://www.sparkfun.com/datasheets/Wireless/Zigbee/XBee-2.5-Manual.pdf
+  * section 2.1.3 (page 12).
+  */
+ public:
+  SerialFlow(int baud, int flow_pin) : flow_pin_(flow_pin), baud_(baud) {}
+  void init() {
+    Serial.begin(baud_);
+    pinMode(flow_pin_, INPUT);
+  }
+  template <typename T>
+  void print(T data) {
+    // Note: if data is too long, the cts pin might activate
+    // while serial is printing and we will end up ignoring it.
+    // The xbee can take up to 34 bytes after the cts pin clears.
+    while (!cts()) continue;
+    Serial.print(data);
+  }
+  void write(const char *buf, int n) {
+    for (int i = 0; i < n; ++i) {
+      while (!cts()) continue;
+      Serial.write(buf[n]);
+    }
+  }
+ private:
+  const int flow_pin_;
+  const int baud_;
+  bool cts() { return !digitalRead(flow_pin_); }
+};
--- sensor_test/sensor_test.ino
+++ sensor_test/sensor_test.ino
@@ -1,88 +1,136 @@
 #include "Arduino.h"
 #include "PPM.h"
 #include <Wire.h>
+#include <Servo.h>
+#include "SerialFlow.h"
+#include "Accel.h"
+#include "Wind.h"
 
 int compassAddress = 0x80; // From datasheet compass address is 0x42
 // shift the address 1 bit right, the Wire library only needs the 7
 // most significant bits for the address
 int reading = 0;
 
-static const char pow_pin = 14;
+int flw_ctl_pin = 5;
 PPM pins(2);
+SerialFlow serial(9600, flw_ctl_pin);
+Wind wind(compassAddress);
+Accel accel(1, 2, 3);
+Servo winch, rudder;
+int winch_pin, rudder_pin;
+int winch_channel = 1, rudder_channel = 2; // PPM channels. TODO: Figure out which channels to use.
+
+int auto_winch = 0;
+
 void setup() {
-  Serial.begin(9600);
+  serial.init();
+  wind.init();
+  // Roughly speaking, 1700 is all the way in for the winch.
+  // 1200 is all the way out (give or take).
+  winch.attach(winch_pin, 1200, 1700);
+  rudder.attach(rudder_pin, 500, 2500);
 
   // Compass sensor setup.
   Wire.begin();       // join i2c bus (address optional for master)
+  if (true) {
+    // Do compass calibration.
+    Wire.beginTransmission(compassAddress);
+    Wire.write('C');
+    Wire.endTransmission();
+    for (int i = 0; i < 25; ++i) {
+      Serial.println(i);
+      delay(1000);
+    }
+    Wire.beginTransmission(compassAddress);
+    Wire.write('E');
+    Wire.endTransmission();
+  }
   Serial.println("Hello World");
   pinMode(12, OUTPUT);
   pinMode(13, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
+  pinMode(flw_ctl_pin, INPUT);
   digitalWrite(12, HIGH); // Power supply for wind direction sensor
   digitalWrite(13, LOW); // Power supply for wind direction sensor
   digitalWrite(3, HIGH); // Power supply for GPS
   digitalWrite(4, LOW); // Power supply for GPS
 }
 
-void loop() {
-  bool loop_run = true;
+void print_gps() {
+  // Print whenever a GPRMC message arrives.
+  char buf[128];
+  int ind = 0;
+  bool up_index = false;
   if (Serial.available()) {
-    while (loop_run) {
+    while (true) {
       while (!Serial.available()) continue;
       char chr = Serial.read();
-      Serial.print(char(chr));
-      if (chr == '\n' && !Serial.available()) loop_run = false;
+      if (chr == '$') {
+        up_index = true;
+        ind = 0;
+      }
+      if (ind == 5) {
+        if(strncmp(buf, "$GPRMC", 6)) {
+          // Strings are different; retry.
+          ind = 0;
+          up_index = false;
+        }
+      }
+      buf[ind] = char(chr);
+      if (up_index) ++ind;
+      if (chr == '\n' && !Serial.available()) break;
     }
   }
+  if (ind) serial.write(buf, ind);
+}
 
-  Serial.print("PPM: ");
-  Serial.print(pins.getChannel(0));
-  Serial.print(" ");
-  Serial.print(pins.getChannel(1));
-  Serial.print(" ");
-  Serial.print(pins.getChannel(2));
-  Serial.print(" ");
-  Serial.print(pins.getChannel(3));
-  Serial.print(" ");
-  Serial.print(pins.getChannel(4));
-  Serial.print(" ");
-  Serial.println(pins.getChannel(5));
+void loop() {
+  bool loop_run = true;
+  print_gps();
+
+  serial.print("PPM: ");
+  serial.print(pins.getChannel(0));
+  serial.print(" ");
+  serial.print(pins.getChannel(1));
+  serial.print(" ");
+  serial.print(pins.getChannel(2));
+  serial.print(" ");
+  serial.print(pins.getChannel(3));
+  serial.print(" ");
+  serial.print(pins.getChannel(4));
+  serial.print(" ");
+  serial.print(pins.getChannel(5));
+  serial.print('\n');
+
+  rudder.write(pins.getChannel(rudder_channel));
+
+  if (auto_winch) {
+    if (reading != -1) {
+      // Basically, we just say that if we are going into the wind, we pull the
+      // sails all the way in; if the wind is right behind us we go all out, and
+      // we vary automatically in between.
+      int abs_reading = reading;
+      //float point = (abs_reading - min_read) / (max_read - min_read);
+      //winch.write(point * 180);
+    }
+  } else {
+    winch.write(pins.getChannel(winch_channel));
+  }
 
   // Do wind sensor stuff.
-  // step 1: instruct sensor to read echoes
-  Wire.beginTransmission(compassAddress);  // transmit to device
-  // the address specified in the datasheet is 66 (0x42)
-  // but i2c adressing uses the high 7 bits so it's 33
-  Wire.write('A');        // command sensor to measure angle  (A)
-  Wire.endTransmission(); // stop transmitting
-
-  // step 2: wait for readings to happen
-  delay(10); // datasheet suggests at least 6000 microseconds
-
-  // step 3: request reading from sensor
-  Wire.requestFrom(compassAddress, 2); // request 2 bytes from slave device #33
-
-  Serial.print("Analog Ports:");
-  for (int i = 0; i < 6; ++i) {
-    Serial.print(" ");
-    Serial.print(analogRead(i));
-  }
-  Serial.println();
-
-  // step 4: receive reading from sensor
-  Serial.print("Wind: ");
-  if (2 <= Wire.available()) // if two bytes were received
-  {
-    reading = Wire.read();  // receive high byte (overwrites previous reading)
-    reading = reading << 8; // shift high byte to be high 8 bits
-    reading += Wire.read(); // receive low byte as lower 8 bits
-    reading /= 10;
-    Serial.println(reading); // print the reading
+  reading = wind.get_reading();
+  if (reading != -1) {
+    serial.print(reading); // print the reading
   } else {
-    Serial.println("Data not available");
+    serial.print("Data not available");
   }
+  serial.print('\n');
 
-  delay(200);
+  serial.print("Heel: ");
+  serial.print(accel.Heel());
+  serial.print('\n');
+
+  delay(100);
 
 }
